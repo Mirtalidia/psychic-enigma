c2f8fdb901cac590ebc006b3493595082dce9d3a
Mirtalidia <142640145+Mirtalidia@users.noreply.github.com>
2023-09-26 15:58:16 -0300

    Add files via upload

---
+++ grid-vs-flexbox-codigo_base/js/app.js
@@ -0,0 +1,18 @@
+// es un comentario
+//ecmascript 6
+// supongamos q quiero dolar por rupias en resultado
+//declaro variables y multiplico
+var rupias=100
+var dolar_conversion_rupia
+dolar_conversion_rupia=0.0012
+var resultado
+resultado=rupias*dolar_conversion_rupia
+alert("$"+rupias+"serian $"+resultado+"usd")
+console.log("hola mundo")
+var nacio= 1993
+var hoy= 2023
+var edad= hoy-nacio
+// tengo q saber si es >18
+if (edad>18){
+    alert ("bienvenido")}
+    else {console.log("no podes sos menor")}
